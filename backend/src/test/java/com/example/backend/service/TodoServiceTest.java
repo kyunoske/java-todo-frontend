e3f4cef7702de7b9eb823419869ed050bdffd2bd
ryan shade <kyunoske@gmail.com>
2022-09-21 09:43:04 +0200

    move pom to root folder

--- backend/src/test/java/com/example/backend/service/TodoServiceTest.java
+++ backend/src/test/java/com/example/backend/service/TodoServiceTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,24 +27,49 @@ class TodoServiceTest {
 
     TodoRepo repo = mock(TodoRepo.class);
     TodoService service = new TodoService(repo);
-    TodoList list = new TodoList();
+
+    @Test
+    void getAllTodos_ReturnsAllTodos() {
+        //Given
+        TodoModel todo1 = new TodoModel();
+        TodoModel todo2 = new TodoModel();
+        List<TodoModel> todoList = new ArrayList<>(List.of(todo1, todo2));
+        when(repo.getAllTodos()).thenReturn(todoList);
+
+        //When
+        List<TodoModel> actual = service.getAllTodos();
+
+        //Then
+        assertEquals(List.of(todo1, todo2), actual);
+    }
+
+    @Test
+    void addNewTodo() {
+        //Given
+        TodoModel todo1 = new TodoModel("1", "OPEN", "test");
+
+
+        when(repo.postNewTodo(todo1)).thenReturn(todo1);
+
+        //When
+        TodoModel actual = service.addTodo(todo1);
+
+        //Then
+        assertEquals(todo1, actual);
+    }
 
 //    @Test
-//    void getAllTodos_ReturnsAllTodos() {
+//    void getTodoById() {
 //        //Given
-//        Map<String, TodoModel> toDos = new HashMap<>(
-//                Map.of(
-//                        "1", new TodoModel("1", "OPEN","Wash the car"),
-//                        "2", new TodoModel("2", "OPEN","Sing a song"),
-//                        "3", new TodoModel("3", "DONE","Write a poem"))
-//        );
-//
-//        when(repo.getAllTodos()).thenReturn();
+//        TodoModel todo1 = new TodoModel("1", "OPEN", "test1");
+//        TodoModel todo2 = new TodoModel("2", "OPEN", "test2");
+//        List<TodoModel> todoList = new ArrayList<>(List.of(todo1, todo2));
+//        when(repo.getAllTodos()).thenReturn(todoList);
 //
 //        //When
-//        List<TodoModel> actual = service.getAllTodos();
+//        TodoModel actual = service.getTodoById("2");
 //
 //        //Then
-//        assertEquals(toDos, actual);
+//        assertEquals(todo2, actual);
 //    }
 }
